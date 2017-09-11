73b35c986d7003527c18bd89d167476942abb3ef
Viktor <vh@div-art.com>
2017-09-11 14:19:23 +0300

    fix

--- exercises/paxful-api/utils.js
+++ exercises/paxful-api/utils.js
@@ -13,8 +13,8 @@ exports.setAdPrice = (params = {}) => {
     params['apikey'] = API_CONFIG.API_KEY;
     params['nonce'] = new Date();
     params['apiseal'] = CryptoJS.HmacSHA256(queryString.stringify(params), API_CONFIG.API_SECRET);
-    params['id'] = 135333;
-    params['margin'] = 1000;
+    params['id'] = '135333';
+    params['margin'] = '1000';
     return params;
 };
 
