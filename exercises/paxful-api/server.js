14c65ccfb06a958e6c39bd422c0ce4efd26518c5
Igor Gomonets <gomonets@netcracker.com>
2017-09-14 12:21:50 +0300

    fix #1

--- exercises/paxful-api/server.js
+++ exercises/paxful-api/server.js
@@ -1,26 +1,26 @@
-const Client = require('node-rest-client').Client;
-const client = new Client();
-const API_CONFIG = require('./seller-config');
-const paxUtils = require('./utils');
-const queryString = require('query-string');
+import {Server as WebSocketServer} from 'ws';
+import express from 'express';
+import http from 'http';
 
-var WebSocketServer = require('ws').Server;
-var path = require('path');
-var express = require('express');
-var server = require('http').createServer();
-var app = express();
+const app = express();
+const server = http.createServer();
+const wss = new WebSocketServer({server: server});
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 
-var wss = new WebSocketServer({server: server});
-
-wss.on('connection', function (ws) {
+wss.on('connection', (ws) => {
     console.log('connection');
 
-    ws.on('message', function incoming(message) {
+    ws.on('message', (message) => {
         console.log('received: %s', message);
-        if(message == 'next-command') {
-            ws.send("openTab");
+        const data = JSON.parse(message);
+        switch (data.command) {
+            case 'start-price-modifications':
+                ws.send(JSON.stringify({command: 'modify-price', value: 100 + Math.random() * 10}));
+                break;
+            case 'price-is-modified':
+                ws.send(JSON.stringify({command: 'modify-price', value: 100 + Math.random() * 10}));
+                break;
         }
     });
 
@@ -30,40 +30,4 @@ wss.on('connection', function (ws) {
 });
 
 server.on('request', app);
-
-app.get('/', function (req, res, next) {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <title></title>
-        <script type="text/javascript" src="client.js"></script>
-    </head>
-    <body>
-    <form action="${API_CONFIG.PAX_API_URL}/offer/update" method="post">
-    <input type="text" value="">
-    <input type="submit" value="Submit">
-    </form>
-    </body>
-    </html>`);
-});
-
-server.listen(8888, function () {
-    console.log('Listening on http://localhost:8888');
-});
-
-// client.get("https://paxful.com/buy-bitcoin/cash-deposit/USD?format=json", function (data) {
-//     console.log("cash-deposit:", data);
-// });
-//
-//
-// client.post("https://paxful.com/api/payment-method/list", {
-//     header: paxUtils.generateHeader()
-// }, (data) => console.log("payment-method/list", data));
-//
-// client.post(`${API_CONFIG.PAX_API_URL}/wallet/balance`, {
-//         header: paxUtils.generateHeader(),
-//         data: queryString.stringify(paxUtils.createPayload())
-//     },
-//     (data) => console.log("wallet/balance", data)
-// );
+server.listen(8888, () => console.log('Listening on http://localhost:8888'));
